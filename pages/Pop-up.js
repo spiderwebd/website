556b7c6d0d8291fd5c0d2417280179de0bb08e74
SwiftlyBox <paymentsswiftlybox@gmail.com>
2023-01-26 16:40:04 +0200

    me

---
+++ pages/Pop-up.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Popup =()=>{
+    return (
+        <>
+        Hey
+        </>
+    )
+}
+export default Popup;
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function contact() {
+  return (
+    <div>
+      Hey
+    </div>
+  )
+}
+
+export default contact;
